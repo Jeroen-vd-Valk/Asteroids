8d889219ca2fad28c3e4ee98ce0a2257fdaa9f7f
Jeroen-vd-Valk <731141@student.inholland.nl>
2025-08-22 17:21:06 -0400

    Added a player character and made it movable using groups

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
-
+from player import Player
 
 def main():
-    
+    # create some groups and add all Player classes to it
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     print("Starting Asteroids!")
     pygame.init
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    # these are variables needed for the game to work
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    time = pygame.time.Clock()
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))
+        screen.fill((0,0,0))        # renders the background
 
-        pygame.display.flip()
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)         # renders the player's model
+
+        pygame.display.flip()       # flips the screen
+        dt = time.tick(60) / 1000   # delays to 60 fps
 
 
 
