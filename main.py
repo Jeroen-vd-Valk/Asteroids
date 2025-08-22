ffb8eaf1924e54fb4a399128a4a00572330e6058
Jeroen-vd-Valk <731141@student.inholland.nl>
2025-08-22 15:44:27 -0400

    Created the files for the game

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+
+
+def main():
+    
+    print("Starting Asteroids!")
+    pygame.init
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while (True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
